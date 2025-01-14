e74fa9745e354522204901cf2e2b0ec2b09eec00
gekkoha <teshamai12@gmail.com>
2025-01-14 05:00:11 +0200

    234

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 function getElementWidth(content, padding, border) {
-    const content = parseFloat(contentWidth);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-    const totalWidth = content + 2 * paddingValue + 2 * borderValue;
+  const content = parseFloat(contentWidth);
+  const paddingValue = parseFloat(padding);
+  const borderValue = parseFloat(border);
+  const totalWidth = content + 2 * paddingValue + 2 * borderValue;
 
-    return totalWidth;
-};
-console.log(getElementWidth("50px", "8px", "4px"));     // 74
-console.log(getElementWidth("60px", "12px", "8.5px"));  // 101
-console.log(getElementWidth("200px", "0px", "0px"));    // 200
+  return totalWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
